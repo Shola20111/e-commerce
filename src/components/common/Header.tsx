64456e3939b7872260c846aee95b63c018fb5e84
Shola20111 <bigt4prigress20111@gmail.com>
2025-09-03 11:31:50 +0100

    Implemented Categories section and some updates

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,58 +1,3 @@
-// "use client"
-// import Image from 'next/image'
-// import Link from 'next/link'
-// import React from 'react'
-// import { BsCart3 } from 'react-icons/bs'
-// import { CiHeart } from 'react-icons/ci'
-// import SearchBar from './SearchBar'
-// import { usePathname } from 'next/navigation'
-// import Navbutton from './Navbutton'
-
-
-
-// const Header = () => {
-
-//   const pathname = usePathname();
-
-//   const links = [
-//     { href: "/", label: "Home" },
-//     { href: "/contact", label: "Contact" },
-//     { href: "/about", label: "About" },
-//     { href: "/signUp", label: "Sign Up" },
-//   ]
-//   return (
-//     <div className='px-[135px] w-full h-[50px] mt-[40px] pb-[15px] border border-[#e4e0e0] mx-auto flex justify-between items-center'>
-//       <div className='border w-[700px] h-[100%] flex items-center justify-between'>
-//         <div className='flex items-center w-[220px] h-full '>
-//           <Image src='/logo.png' width={200} height={20} alt='Logo'/>
-//         </div>
-//         <div className="w-[370px] h-full flex justify-center items-center gap-[40px] text-[18px] md:text-lg">
-//       {links.map((link) => (
-//         <Link
-//           key={link.href}
-//           href={link.href}
-//           className={pathname === link.href ? "underline" : ""}
-//         >
-//           <Navbutton text={link.label} />
-//         </Link>
-//       ))}
-//     </div>
-//       </div>
-     
-//       <div className='w-[347px] h-full flex items-center justify-between'>
-//         <SearchBar/>
-//         <div className='w-[80px] h-full flex gap-[16px] '>
-//           <CiHeart className='w-[25px] h-[25px]'/>
-//           <BsCart3 className='w-[25px] h-[20px]'/>
-//         </div>
-        
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Header
-
 "use client";
 
 import Image from "next/image";
@@ -77,14 +22,14 @@ const Header = () => {
 
   return (
     <div className="px-[10px] md:px-[50px] lg:px-[135px] w-full h-[60px] mt-[20px] md:mt-[40px] pb-[10px] md:pb-[15px] border border-[#e4e0e0] mx-auto flex justify-between items-center">
-      {/* Left Section */}
+      
       <div className="flex items-center justify-between w-full md:w-[700px] h-full">
-        {/* Logo */}
+       
         <div className="flex items-center w-[220px] md:w-[150px] h-full">
           <Image src="/logo.png" width={200} height={20} alt="Logo" />
         </div>
 
-        {/* Links (desktop only) */}
+       
         <div className="hidden lg:flex  w-full h-full justify-center items-center gap-[40px] text-[18px] md:text-lg">
           {links.map((link) => (
             <Link
@@ -98,20 +43,20 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Right Section */}
+      
       <div className="flex items-center gap-4">
-        {/* SearchBar hidden on mobile */}
+        
         <div className="hidden md:block">
           <SearchBar />
         </div>
 
-        {/* Icons */}
+        
         <div className="flex gap-[16px]">
           <CiHeart className="w-[25px] h-[25px]" />
           <BsCart3 className="w-[25px] h-[20px]" />
         </div>
 
-        {/* Mobile Hamburger */}
+        
         <div className="md:hidden">
           <button onClick={() => setMenuOpen(true)}>
             <svg
@@ -131,14 +76,14 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Mobile Menu Drawer */}
+     
       {menuOpen && (
         <div
           className="fixed inset-0 bg-white z-50 md:hidden"
           onClick={() => setMenuOpen(false)}
         >
           
-            {/* Close Button */}
+            
             <div className="w-full h- flex justify-end p-5">
               <button className="text-[30px]" onClick={() => setMenuOpen(false)}>
               X
@@ -148,7 +93,7 @@ const Header = () => {
 
           
 
-            {/* Links */}
+            
             <nav className="flex flex-col justify-center items-center gap-4 text-lg">
               {links.map((link) => (
                 <Link
